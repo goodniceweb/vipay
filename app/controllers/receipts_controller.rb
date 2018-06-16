38b71785c269f03a697925228081c5da8d39a492
Alexey Cherkashin <goodniceweb@gmail.com>
2018-06-16 21:19:29 +0300

    FIx

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -4,6 +4,14 @@ class ReceiptsController < ApplicationController
   end
 
   def create
-    render json: { party_id: 1, total: 245.60 }
+    render json: {
+      party_id: 1,
+      total: 245.60,
+      items: [
+        { id: 1, amount: 2, name: "potato", price: "50", total: "100" },
+        { id: 2, amount: 1, name: "cola", price: "5.60", total: "5.60" },
+        { id: 3, amount: 1, name: "pizza", price: "40", total: "40" }
+      ]
+    }
   end
 end
