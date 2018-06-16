1cb904af2fa369e2c295d16b0d14752868d8980f
Alexey Cherkashin <goodniceweb@gmail.com>
2018-06-16 22:03:17 +0300

    Fix

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,7 +1,7 @@
 class ParticipantsController < ActionController::Base
   def create
     party = Party.find_by(id: params[:party_id])
-    user = User.where(phone: params[:phone]).first_or_create
+    user = User.where(phone: params[:telegram_id]).first_or_create
     if user.present? && party.present?
       participant = Participant.new(user_id: user.id, party_id: party.id)
       if participant.save
@@ -16,7 +16,7 @@ class ParticipantsController < ActionController::Base
 
   def remove
     party = Party.find_by(id: params[:party_id])
-    user = User.find_by(phone: params[:phone])
+    user = User.find_by(phone: params[:telegram_id])
     if user.present? && party.present?
       Participant.where(user_id: user.id, party_id: party.id).destroy_all
       render json: party.as_json
