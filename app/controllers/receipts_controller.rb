dd884265f1a1058ce895c6d35428082541f876a9
Alexey Cherkashin <goodniceweb@gmail.com>
2018-06-17 04:33:52 +0300

    Fix

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,10 +1,17 @@
+require "google/cloud/vision"
+
 class ReceiptsController < ApplicationController
   def index
     render json: { shit: true }
   end
 
   def create
-    # TODO: create items
+    path = Rails.root.join("shit.jpg")
+    File.open(path, "wb") { |f| f.write Base64.decode64(params[:image])  }
+    vision = Google::Cloud::Vision.new
+    image = vision.image path
+    text = image.text
+    puts text
     render json: Party.where(chat_id: -220834405).last.as_json
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   def nickname
     return "@#{username}" if username.present?
-    username.first_name
+    first_name
+  end
+
+  def build_invoice_for(party)
+    participant = Participant.find_by(user_id: id, party_id: party.id)
+    return if participant.blank?
+    invoice = Invoice.new(participant_id: participant.id, paid: false)
+    items = ParticipantItem.joins(item: :party).where(parties: { id: party.id }).where(participant_id: participant.id)
+    return if items.blank?
+    total = items.map(&:total).map(&:to_f).reduce(:+)
+    invoice.total = total
+    return unless invoice.save
+    items.update_all(invoice_id: invoice.id)
+    invoice
   end
 end
